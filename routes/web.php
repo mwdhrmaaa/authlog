98f10fcc9e2414e05646db44ee02f80dc748d1b1
mwdhrmaaa <mwdhrmaaa@gmail.com>
2026-01-17 15:13:10 +0800

    Define Google Auth routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Http\Controllers\Public\Auth\GoogleController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::prefix('auth')->name('public.auth.')->group(function () {
+    Route::get('login', function () {
+        return view('public.auth.login');
+    })->name('login');
+    Route::get('google/redirect', [GoogleController::class, 'redirectToGoogle'])->name('google.redirect');
+    Route::get('google/callback', [GoogleController::class, 'handleGoogleCallback'])->name('google.callback');
 });
