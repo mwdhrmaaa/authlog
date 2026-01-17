fad5a9a12b73549b7c0be812d8043dbaa60211e1
mwdhrmaaa <mwdhrmaaa@gmail.com>
2026-01-17 15:13:10 +0800

    Update User model with Google Auth fillable fields

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,10 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'google_id',
+        'google_token',
+        'google_refresh_token',
+        'avatar',
     ];
 
     /**
