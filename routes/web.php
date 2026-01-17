ed49fa436a80391e860e413d1264b449a945ec27
mwdhrmaaa <mwdhrmaaa@gmail.com>
2026-01-17 16:12:56 +0800

    Define dashboard and logout routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Member\DashboardController;
 use App\Http\Controllers\Public\Auth\GoogleController;
 use Illuminate\Support\Facades\Route;
 
@@ -14,3 +15,12 @@ Route::prefix('auth')->name('public.auth.')->group(function () {
     Route::get('google/redirect', [GoogleController::class, 'redirectToGoogle'])->name('google.redirect');
     Route::get('google/callback', [GoogleController::class, 'handleGoogleCallback'])->name('google.callback');
 });
+
+Route::middleware(['auth'])->name('member.')->group(function () {
+    Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
+});
+
+Route::post('logout', function () {
+    Auth::logout();
+    return redirect('/');
+})->name('logout');
